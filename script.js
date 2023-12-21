366eb60743e38f738e4cd30030cc3560bb34bf5b
Misbahur rahman <98620184+Misbahrahman@users.noreply.github.com>
2023-12-21 19:31:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function makeid(l) {
-  // write your code here
+  let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let ans = "";
+  for(let i = 0 ; i < l ; i++){
+	  ans += str.charAt((Math.random() * str.length) % str.length);
+  }
+
+	return ans;
+	
 }
 
 // Do not change the code below.
